f09cec11f5520becec34d97a6dfeb09f0fb6d8e2
yg2356 <kmnfcg@naver.com>
2022-10-21 16:27:54 +0900

    inital

---
+++ github_test/src/test/java/com/example/github_test/GithubTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.github_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
